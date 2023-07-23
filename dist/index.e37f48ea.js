078e5717ebb5d2f3b0e2c2f6e59ee3b4e1eb1713
Azikenp <azikenp26@gmail.com>
2023-07-23 12:32:00 +0100

    success mesage

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -722,6 +722,7 @@ class RecipeView {
     #parentElement = document.querySelector(".recipe");
     #data;
     #errorMessage = "We could not find that recipe. Please find another one.";
+    #message = "";
     render(data) {
         this.#data = data;
         const markup = this.#generateMarkup();
@@ -748,6 +749,20 @@ class RecipeView {
                 <div>
                     <svg>
                         <use href="${(0, _iconsSvgDefault.default)}#icon-alert-triangle"></use>
+                    </svg>
+                </div>
+                <p>${message}</p>
+            </div>
+        `;
+        this.#clear();
+        this.#parentElement.insertAdjacentHTML("afterbegin", markup);
+    }
+    renderMessage(message = this.#message) {
+        const markup = `
+            <div class="message">
+                <div>
+                    <svg>
+                        <use href="${(0, _iconsSvgDefault.default)}#icon-smile"></use>
                     </svg>
                 </div>
                 <p>${message}</p>
